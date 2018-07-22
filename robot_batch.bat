b46d85a02ee6eb6c08a2c8f2410e548b6009543a
sandeepbtm <32632384+sandeepbtm@users.noreply.github.com>
2018-07-22 13:27:06 +0800

    Delete robot_batch.bat

--- robot_batch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set "script_path=D:\PyCharm_Workspace\REA-Toy-Robot\robot_project\"
-set "script_path=%script_path%__main__.py"
-
-echo %script_path%
-
-python %script_path% input_data %*
